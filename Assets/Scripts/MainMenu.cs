19925f8830478ad2b56e21448d83931576bb8f1c
nwicakson <nugroho13@mhs.if.its.ac.id>
2016-12-11 13:18:51 +0700

    Initial Commit
    - Add first design
    - Add vuforia library
    - Add elephant and gorilla object

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioClip sfx;
+    public AudioClip backgroundMusic;
+    private SoundManager soundManager;
+
+    void Awake()
+    {
+        //TextAsset file = Resources.Load<TextAsset>("InitialSetting");
+        //prettyJson = JsonMapper.ToObject(file.text);
+    }
+
+    void Start()
+    {
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        soundManager.PlayBackground(backgroundMusic);
+    }
+
+    public void Play()
+    {
+        soundManager.PlaySoundEffect(sfx);
+        SceneManager.LoadScene("Play");
+    }
+
+    public void Setting()
+    {
+        soundManager.PlaySoundEffect(sfx);
+        SceneManager.LoadScene("Setting");
+    }
+
+    public void Exit()
+    {
+        soundManager.PlaySoundEffect(sfx);
+        PlayerPrefs.Save();
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource backgroundMusic;
+    public AudioSource soundEffect;
+    private static SoundManager instance = null;
+    private float lowPitchRange = .95f, highPitchRange = 1.05f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayBackground(AudioClip clip)
+    {
+        backgroundMusic.clip = clip;
+        backgroundMusic.Play();
+    }
+
+    public void PlaySoundEffect(AudioClip clip)
+    {
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+        soundEffect.pitch = randomPitch;
+        soundEffect.clip = clip;
+        soundEffect.Play();
+    }
+
+}
